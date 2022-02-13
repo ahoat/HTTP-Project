2d2dacd9dfd47d00f0d8ad485b048a2613772faf
= <maria.louisa.castro@gmail.com>
2022-02-13 19:24:47 +0000

    Link buttons to JS, onclick each button fetchs API with value of category
    Realtes #2

--- scripts.js
+++ scripts.js
@@ -1,25 +1,29 @@
 // News API call
-let category = "top-headlines";
+let category = "entertainment";
 const newsKey = `39c143627cee481882724a0a7fa661f1`;
-const newsUrl = `https://newsapi.org/v2/${category}?country=gb&apiKey=${newsKey}`;
-const categorybuttons = Array.from(document.querySelectorAll(".category"));
 
 function getNews() {
-  fetch(newsUrl)
+  fetch(
+    `https://newsapi.org/v2/top-headlines?country=gb&category=${category}&apiKey=${newsKey}`
+  )
     .then((response) => {
       if (!response.ok) throw new Error(response.status);
       return response.json();
     })
     .then((data) => {
-      console.log(data.articles[0].title);
-      console.log(data.articles[0].urlToImage);
-      console.log(data.articles[0].content);
+      console.log(data);
+      //  console.log(data.articles[0].title);
+      //  console.log(data.articles[0].urlToImage);
+      //  console.log(data.articles[0].content);
     })
     .catch((error) => console.log(error));
 }
 
+const categorybuttons = Array.from(document.querySelectorAll(".category"));
+
 categorybuttons.forEach((button) =>
   button.addEventListener("click", (e) => {
-    console.log(e.target.value);
+    category = e.target.value;
+    getNews();
   })
 );
