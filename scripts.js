889fadd3a5ef9d6c5da8f8bb2dec79314862cb95
= <maria.louisa.castro@gmail.com>
2022-02-13 23:50:56 +0000

    Add CSS to news response and create header
    Relates #2 and Relates #3

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 // News API call
-const newsTitle = document.querySelector(".news-title");
-const newsImage = document.querySelector(".news-img");
+const newsTitle = Array.from(document.querySelectorAll(".news-title"));
+const newsImage = Array.from(document.querySelectorAll(".news-img"));
 let img;
-const newsArtile = document.querySelector(".news-article");
-const newsLink = document.querySelector(".news-link");
+const newsArtile = Array.from(document.querySelectorAll(".news-article"));
+const newsLink = Array.from(document.querySelectorAll(".news-link"));
+
+const newsSubTitle = document.querySelector(".top-x-stories");
 
 let category = "entertainment";
 const newsKey = `39c143627cee481882724a0a7fa661f1`;
@@ -17,16 +19,24 @@ function getNews() {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
-      newsTitle.innerHTML = data.articles[2].title;
-      if (newsImage.hasChildNodes() === false) {
-        img = new Image(200, 100);
-        newsImage.appendChild(img);
-      }
-      img.src = data.articles[2].urlToImage;
-      newsArtile.innerHTML = data.articles[2].content;
-      newsLink.innerHTML = "Link to full article";
-      newsLink.href = data.articles[2].url;
+      newsSubTitle.innerHTML = `Top ${category} news`;
+      newsTitle.forEach((title, index) => {
+        title.innerHTML = data.articles[index].title;
+      });
+      newsImage.forEach((image, index) => {
+        if (image.hasChildNodes() === false) {
+          img = new Image(200);
+          image.appendChild(img);
+          img.src = data.articles[index].urlToImage;
+        } else img.src = data.articles[index].urlToImage;
+      });
+      newsArtile.forEach((news, index) => {
+        news.innerHTML = data.articles[index].description;
+      });
+      newsLink.forEach((link, index) => {
+        link.innerHTML = "Link to full article";
+        newsLink.href = data.articles[index].url;
+      });
     })
     .catch((error) => console.log(error));
 }
@@ -39,3 +49,5 @@ categorybuttons.forEach((button) =>
     getNews();
   })
 );
+
+//getNews();
